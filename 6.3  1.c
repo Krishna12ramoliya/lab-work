864c02f0b1f8445383a1cf4877dccfb2996db1ab
krishna ramoliya <ramoliyakrishna858@gmail.com>
2025-04-25 16:55:34 +0530

    helo

---
+++ 6.3  1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,sum=0;
+	
+	for(i=1;i<=5;i++)
+	{
+		printf("%d ",i);
+		sum=sum+i;
+	}
+	printf("total sum:%d",sum);
+	
+}
+
+
+/* output:1 2 3 4 5 total sum:15
+*/
